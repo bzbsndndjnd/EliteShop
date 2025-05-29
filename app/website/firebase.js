8c9bb77325f7ad8fad098a41e7e6762f8c20c945
Shamia <mha16576@gmail.com>
2025-05-29 08:59:24 +0300

    initial commit

--- app/website/firebase.js
+++ app/website/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     authDomain: "website-e3714.firebaseapp.com",
     databaseURL: "https://website-e3714-default-rtdb.firebaseio.com/",
     projectId: "website-e3714",
-    storageBucket: "website-e3714.firebasestorage.app",
+    storageBucket: "website-e3714.appspot.com",
     messagingSenderId: "939847034403",
     appId: "1:939847034403:web:a868ee5d8e25f9304eb396",
     measurementId: "G-Q2S34JVV59"
